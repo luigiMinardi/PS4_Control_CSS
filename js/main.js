18b6a04747d057f97c1f5140726ee3925e04e6ee
Luigi Minardi <lugminsinlag@gmail.com>
2022-01-23 20:46:11 +0100

    Adding the images of the ps4 starting and doing the js funciong related to it

--- js/main.js
+++ js/main.js
@@ -32,10 +32,20 @@ function bgRed() {
 }
 function showExample() {
     let example = document.getElementById('example')
-    if (example.style.display == "unset") {
-        example.style.display = "none";
+    let hdmi = document.getElementById('hdmi')
+    let hdmiText = document.getElementById('hdmiText')
+    if (hdmi.style.display == "flex") {
+        alert("Turn on the ps4 before clicking to see the options")
     } else {
-        example.style.display = "unset";
+        if (example.style.display == "unset") {
+            example.style.display = "none";
+            PS4Start.style.display = "unset";
+        } else {
+            hdmi.style.display = "none";
+            hdmiText.style.display = "none";
+            example.style.display = "unset";
+            PS4Start.style.display = "none";
+        }
     }
 }
 function copyURL() {
@@ -45,9 +55,22 @@ function copyURL() {
 }
 function turnOn() {
     let touchpad = document.getElementById('touchpad')
+    let PS4Start = document.getElementById('PS4Start')
+    let hdmi = document.getElementById('hdmi')
+    let hdmiText = document.getElementById('hdmiText')
     if (touchpad.style.background == "linear-gradient(0deg, black 70%, blue 78%, gray 90%, black 100%)") {
         touchpad.style.background = "linear-gradient(0deg, black 70%, black 78%, gray 90%, black 100%)";
+        PS4Start.style.display = "none";
+        hdmi.style.display = "flex";
+        hdmiText.style.display = "flex";
+        PS4Start.src = "img/PS4_start.gif";
     } else {
         touchpad.style.background = "linear-gradient(0deg, black 70%, blue 78%, gray 90%, black 100%)";
+        PS4Start.style.display = "unset";
+        hdmi.style.display = "none";
+        hdmiText.style.display = "none";
+        setTimeout(function () {
+            PS4Start.src = "img/PS4_home.jpg";
+        }, 1300);
     }
 }
