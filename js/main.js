536750b1f7bfbe2e6aed09288363d17387eb303c
Luigi Minardi <lugminsinlag@gmail.com>
2022-01-23 22:16:54 +0100

    Fixing typo error on file name

--- js/main.js
+++ js/main.js
@@ -63,14 +63,14 @@ function turnOn() {
         PS4Start.style.display = "none";
         hdmi.style.display = "flex";
         hdmiText.style.display = "flex";
-        PS4Start.src = "img/PS4_start.gif";
+        PS4Start.src = "img/ps4_start.gif";
     } else {
         touchpad.style.background = "linear-gradient(0deg, black 70%, blue 78%, gray 90%, black 100%)";
-        PS4Start.style.display = "unset";
+        ps4Start.style.display = "unset";
         hdmi.style.display = "none";
         hdmiText.style.display = "none";
         setTimeout(function () {
-            PS4Start.src = "img/PS4_home.jpg";
+            PS4Start.src = "img/ps4_home.jpg";
         }, 1300);
     }
 }
